29ddfd2651fc6d2c3a4beac6138b1a0301f24496
Alan Morano <abmorano11@gmail.com>
2024-04-08 00:50:53 +0800

    Implement RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './users-list/users-list.component';
 
+const routes: Routes = [];
 @NgModule({
   declarations: [
     AppComponent,
     UsersListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
